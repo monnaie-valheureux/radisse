de0f27cc6119dd714dfe7d261fc71379c715ce05
michael <michael@estsurinter.net>
2017-07-15 23:52:17 +0200

    Add an Eloquent model for partners of the currency

---
+++ app/Partner.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * A Partner is a person or organization that uses the local currency.
+ */
+class Partner extends Model
+{
+    //
+}
