fb323ae57f5aae7cb9af55c672dc56d0c743eaf6
michael <michael@estsurinter.net>
2017-07-15 23:55:22 +0200

    Add admin route to the list of partners

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,9 @@
 // This group defines the routes used by the administration area of the site.
 Route::prefix('gestion')->namespace('Admin')->group(function () {
 
+    // Define routes to handle partners of the local currency.
+    Route::resource('partners', 'PartnersController', [
+        'only' => ['index']
+    ]);
+
 });
