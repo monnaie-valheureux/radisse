3ae72b05d4a3ee8d165a1002ae95844fde299df2
michael <michael@estsurinter.net>
2017-07-28 12:16:20 +0200

    Automatically generate slug when creating partner

--- app/Partner.php
+++ app/Partner.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -9,5 +10,19 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Partner extends Model
 {
-    //
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        // When a partner is created, we automatically
+        // generate a slug based on its name.
+        self::creating(function (self $partner) {
+            $partner->slug = Str::slug($partner->name);
+        });
+    }
 }
