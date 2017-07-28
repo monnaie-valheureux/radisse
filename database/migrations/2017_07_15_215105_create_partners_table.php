ec86ab0d653a49c334ee6851e82242961fafa134
michael <michael@estsurinter.net>
2017-07-28 12:11:58 +0200

    Add new columns to `partners` table
    This adds the following columns:
    - `name_sort`
    - `slug`
    - `business_type`

--- database/migrations/2017_07_15_215105_create_partners_table.php
+++ database/migrations/2017_07_15_215105_create_partners_table.php
@@ -22,6 +22,20 @@ class CreatePartnersTable extends Migration
             // The name of the partner.
             $table->string('name');
 
+            // The name of the partner, but maybe in a modified form that will
+            // make it easier for people to find it in an alphabetical list.
+            // Example : 'Du côté de chez Poje' → 'Poje (du côté de chez)'
+            $table->string('name_sort')->nullable();
+
+            // The slug is a simplified version of the name that is
+            // both people-friendly and computer-friendly, for use
+            // in places such as URLs or filenames.
+            $table->string('slug');
+
+            // The type of business (ASBL, SPRL, indépendant en
+            // personne physique, etc.)
+            $table->string('business_type')->nullable();
+
             // Timestamps telling when the table row was created
             // and when it was modified for the last time.
             $table->timestamps();
