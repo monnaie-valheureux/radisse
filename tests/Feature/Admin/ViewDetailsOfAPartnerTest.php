c74b5dee39d45a6a9e0815acecadaf9df52982b7
michael <michael@estsurinter.net>
2017-07-28 12:19:44 +0200

    Test ability to see a single partner at a time

---
+++ tests/Feature/Admin/ViewDetailsOfAPartnerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Partner;
+use Tests\TestCase;
+use Illuminate\Support\Str;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ViewDetailsOfAPartnerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function admins_can_see_a_particular_partner()
+    {
+        // Create two partners.
+        $partner = factory(Partner::class)->create([
+            'name' => 'Du côté de chez Poje',
+            'name_sort' => 'Poje (du côté de chez)',
+            'business_type' => 'SPRL',
+        ]);
+        $otherPartner = factory(Partner::class)->create([
+            'name' => 'Boucherie Sanzot',
+        ]);
+
+        $response = $this->get('/gestion/partners/'.$partner->slug);
+
+        // Check that we can see the details of a specific partner.
+        $response->assertSeeText($partner->name);
+        $response->assertSeeText($partner->name_sort);
+        $response->assertSeeText($partner->business_type);
+
+        // Check that we don’t see anything related to the other partner.
+        $response->assertDontSeeText($otherPartner->name);
+    }
+}
