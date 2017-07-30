c8d9d2bfa3d5624c95539e4772d1b91030912a9f
michael <michael@estsurinter.net>
2017-07-30 17:12:55 +0200

    Test that a location can retrieve its partner

---
+++ tests/Unit/LocationTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit\Admin;
+
+use App\Partner;
+use App\Location;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class LocationTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function can_retrieve_its_partner()
+    {
+        $partner = factory(Partner::class)->create();
+
+        $location = factory(Location::class)->create([
+            'partner_id' => $partner->id,
+        ]);
+
+        $retrievedPartner = $location->partner;
+
+        $this->assertSame($partner->id, $retrievedPartner->id);
+    }
+}
