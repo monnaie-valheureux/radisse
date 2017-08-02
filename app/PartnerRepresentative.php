2ac5d2f0833812bbe6c3e3865c1e7a82e274537d
michael <michael@estsurinter.net>
2017-08-03 00:49:54 +0200

    Add an Eloquent model for representatives

---
+++ app/PartnerRepresentative.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * A representative is a person who is officially acting in the name of a
+ * partner. This person is the one who communicates with the organization
+ * managing the currency project.
+ */
+class PartnerRepresentative extends Model
+{
+    /**
+     * Get the partner that this person represents.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function partner()
+    {
+        return $this->belongsTo(Partner::class);
+    }
+}
