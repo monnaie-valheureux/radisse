2233f3184b8317a2115d213789b3a53639c37421
michael <michael@estsurinter.net>
2017-09-18 19:04:07 +0200

    Add main ContactDetails class and related tests

---
+++ app/ContactDetails.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ContactDetails extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * We explicitly define this property here in order to make subclasses
+     * automatically inherit it. This prevents Eloquent to try to guess
+     * the proper table names for subclasses from their names. This
+     * is needed because they all use the contact_details table.
+     *
+     * @var string
+     */
+    protected $table = 'contact_details';
+
+    /**
+     * The type of contact info stored by the object.
+     *
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * Tell if the info is public or if it can only be used internally.
+     *
+     * @var bool
+     */
+    protected $isPublic = false;
+
+    /**
+     * An optional label associated with the contact info.
+     *
+     * @var string
+     */
+    protected $label;
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        // When the contact info is saved to the database, we gather the
+        // data that is related to the ContactDetails object in order
+        // to store it as JSON in a dedicated database column.
+        self::saving(function (self $self) {
+
+            $commonData = [
+                'isPublic' => $self->isPublic,
+                'label' => $self->label,
+            ];
+
+            $specificData = $self->getOwnAttributes();
+
+            // Overwrite the array of attributes of the Eloquent model.
+            $self->attributes = array_merge($self->attributes, [
+                'type' => $self->type,
+                'data' => json_encode($commonData + $specificData),
+            ]);
+        });
+    }
+
+    /**
+     * Get the key-value pairs of data that are specific to this type of contact info.
+     *
+     * This method has to be implemented by subclasses.
+     */
+    protected function getOwnAttributes()
+    {
+        throw new Exception(
+            'getOwnAttributes method must be overwritten by subclasses'
+        );
+    }
+
+    /**
+     * Allow to fluently set the public state a contact info.
+     *
+     * @return self
+     */
+    public function makePublic()
+    {
+        $this->isPublic = true;
+
+        return $this;
+    }
+
+    /**
+     * Allow to fluently set the private state a contact info.
+     *
+     * @return self
+     */
+    public function makePrivate()
+    {
+        $this->isPublic = false;
+
+        return $this;
+    }
+
+    /**
+     * Allow to fluently add a label to a contact info.
+     *
+     * @param  string  $label
+     *
+     * @return self
+     */
+    public function withLabel($label)
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * Allow to read specific properties as if they were public.
+     *
+     * @param  string  $name
+     *
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        switch ($name) {
+            case 'type':
+                return $this->type;
+            case 'isPublic':
+                return $this->isPublic;
+            case 'isPrivate':
+                return !$this->isPublic;
+            case 'label':
+                return $this->label;
+        }
+
+        return parent::__get($name);
+    }
+
+    /**
+     * Allow to write specific properties as if they were public.
+     *
+     * @param string  $name
+     * @param mixed   $value
+     */
+    public function __set($name, $value)
+    {
+        switch ($name) {
+            case 'label':
+                $this->label = ($label = trim($value)) ? $label : null;
+                break;
+            case 'isPublic':
+                $this->isPublic = (bool) $value;
+                break;
+            case 'isPrivate':
+                $this->isPublic = !((bool) $value);
+                break;
+            default:
+                // Fall back on the magic method of the parent.
+                parent::__set($name, $value);
+                break;
+        }
+    }
+
+    /**
+     * Create a new model instance that is existing.
+     *
+     * @param  array  $attributes
+     * @param  string|null  $connection
+     * @return static
+     */
+    public function newFromBuilder($attributes = [], $connection = null)
+    {
+        // First, retrieve the model using the original method.
+        $model = parent::newFromBuilder($attributes, $connection);
+
+        $data = json_decode($model->attributes['data']);
+
+        // Handle properties that are shared by all types of contact details.
+        $model->isPublic = $data->isPublic;
+        $model->label = $data->label;
+
+        // Then, handle specific properties.
+        if (method_exists($model, 'getOwnAttributeKeys')) {
+            $properties = $model->getOwnAttributeKeys();
+        } else {
+            $properties = array_keys($model->getOwnAttributes());
+        }
+
+        foreach ($properties as $property) {
+            $model->{$property} = $data->{$property};
+        }
+
+        return $model;
+    }
+}
