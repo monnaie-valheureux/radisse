13884557ac8b1b5e7b32b5e068a84dfe757bc547
michael <michael@estsurinter.net>
2017-09-25 23:29:16 +0200

    Extract creation of representatives to a method

--- database/seeds/PartnersSeeder.php
+++ database/seeds/PartnersSeeder.php
@@ -17,34 +17,36 @@ class PartnersSeeder extends Seeder
         $sourceFile = database_path('/seeds/data/partners.json');
 
         // Loop on all the partners.
-        foreach (JsonPartnerLoader::load($sourceFile) as $partner) {
-
-            $representatives = [];
-
-            // Loop on all the representatives of each partner.
-            foreach ($partner['representatives'] as $rep) {
-
-                // Skip empty representatives.
-                if (!$rep['given_name'] && !$rep['surname']) {
-                    continue;
-                }
-
-                $representatives[] = PartnerRepresentative::make([
-                    'given_name' => $rep['given_name'],
-                    'surname' => $rep['surname'],
-                    'role' => $rep['role'],
-                    'phone' => $rep['contact_details']['phone'],
-                ]);
-            }
+        foreach (JsonPartnerLoader::load($sourceFile) as $data) {
 
             // Create the partner in the database.
             $partner = Partner::create([
-                'name' => $partner['name'],
-                'name_sort' => $partner['name_sort'],
-                'business_type' => $partner['business_type'],
-            ])
-            // Then, create its representative(s).
-            ->representatives()->saveMany($representatives);
+                'name' => $data['name'],
+                'name_sort' => $data['name_sort'],
+                'business_type' => $data['business_type'],
+            ]);
+
+            $this->addRepresentatives($partner, $data['representatives']);
+        }
+    }
+
+    protected function addRepresentatives(Partner $partner, $reps)
+    {
+        foreach ($reps as $rep) {
+
+            // Skip empty representatives.
+            if (!$rep['given_name'] && !$rep['surname']) {
+                continue;
+            }
+
+            $representative = PartnerRepresentative::make([
+                'given_name' => $rep['given_name'],
+                'surname' => $rep['surname'],
+                'role' => $rep['role'],
+                'phone' => $rep['contact_details']['phone'],
+            ]);
+
+            $partner->representatives()->save($representative);
         }
     }
 }
