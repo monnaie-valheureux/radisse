d35d5587aa4e5e1f639f853303af8636da82bd11
michael <michael@estsurinter.net>
2017-09-26 00:50:45 +0200

    Make Partner use the HasContactDetails trait

--- app/Partner.php
+++ app/Partner.php
@@ -10,6 +10,8 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Partner extends Model
 {
+    use HasContactDetails;
+
     /**
      * The "booting" method of the model.
      *
@@ -62,49 +64,5 @@ class Partner extends Model
     public function representatives()
     {
         return $this->hasMany(PartnerRepresentative::class);
-    }
-
-    /**
-     * Get the partner’s postal addresses.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\MorphOne
-     */
-    public function postalAddress()
-    {
-        return $this->morphOne(PostalAddress::class, 'contactable')
-            ->where('type', 'postal-address');
-    }
-
-    /**
-     * Get all of the partner’s phones.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
-     */
-    public function phones()
-    {
-        return $this->morphMany(Phone::class, 'contactable')
-            ->where('type', 'phone');
-    }
-
-    /**
-     * Get all of the partner’s emails.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
-     */
-    public function emails()
-    {
-        return $this->morphMany(Email::class, 'contactable')
-            ->where('type', 'email');
-    }
-
-    /**
-     * Get all of the partner’s social networks.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
-     */
-    public function socialNetworks()
-    {
-        return $this->morphMany(SocialNetwork::class, 'contactable')
-            ->where('type', 'social-network');
     }
 }
