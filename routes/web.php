7fb3e916e8535659d6e51ef7f26fef22b67014d4
michael <michael@estsurinter.net>
2017-10-18 20:48:30 +0200

    Replace HTTP 307 by a simple route replacement
    If the conditional returns true, we will simply overwrite the route definition instead of redirecting to a new URL.

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,9 @@
 
 Route::view('/', 'public.home');
 
-// If we haven’t launched yet, add a temporary redirect to a teaser page.
-if (\Carbon\Carbon::now() < \Carbon\Carbon::parse('next Saturday 18:00')) {
-    Route::view('/bientot', 'public.launch-teaser');
-    Route::redirect('/', '/bientot', 307);
+// If we haven’t launched yet, redirect home to a teaser page.
+if (\Carbon\Carbon::now()->lessThan(\Carbon\Carbon::parse('next Saturday 18:00'))) {
+    Route::view('/', 'public.launch-teaser');
 }
 
 
