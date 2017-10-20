2906b489fe7300f87d01e4dcd961c3b35dc37c53
michael <michael@estsurinter.net>
2017-10-20 13:00:48 +0200

    Add Piwik tracking code to teaser view

--- resources/views/public/launch-teaser.blade.php
+++ resources/views/public/launch-teaser.blade.php
@@ -7,6 +7,34 @@
     <title>Le Valeureux devient le Val’heureux</title>
 
     <link rel="stylesheet" href="{{ asset('css/teaser.css') }}">
+
+    <!--
+        This sends stats to Piwik, an open source and privacy-friendly
+        analytics tool. We configured it to make it respect privacy
+        even more, by honoring ‘Do Not Track’ headers, not using
+        any cookie and not storing full IP addresses, among
+        other things. Privacy is a human right, period!
+    -->
+    <script type="text/javascript">
+      var _paq = _paq || [];
+      // Do not use cookies.
+      _paq.push(['disableCookies']);
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//stats.valheureux.be/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document,
+            g=d.createElement('script'),
+            s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript';
+        g.async=true;
+        g.defer=true;
+        g.src=u+'piwik.js';
+        s.parentNode.insertBefore(g,s);
+      })();
+    </script>
 </head>
 <body class="launch-teaser">
     <h1>Le Valeureux devient le Val’heureux</h1>
