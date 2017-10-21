c790863599b860b31d1b886b4f593496df463d4a
michael <michael@estsurinter.net>
2017-10-21 18:01:20 +0200

    Remove teaser route hijacking

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,6 @@ Route::namespace('Site')->group(function () {
 });
 
 
-// If we haven’t launched yet (and are not in a local
-// environment), redirect home to a teaser page.
-if (
-    \Carbon\Carbon::now()->lessThan(\Carbon\Carbon::parse('next Saturday 18:00')) &&
-    config('app.env') !== 'local'
-) {
-    Route::view('/', 'public.launch-teaser');
-
-    // Allow to see the home page via a temporary secret route.
-    Route::view('/secret', 'public.home');
-}
-
-
 // This group defines the routes used by the administration area of the site.
 Route::prefix('gestion')->namespace('Admin')->group(function () {
 
