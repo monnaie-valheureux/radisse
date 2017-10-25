b43e217ecccd00f3dedb3c8d3e777ee7c682dbc6
michael <michael@estsurinter.net>
2017-10-25 23:02:57 +0200

    Add factory state for former partners

--- database/factories/Partner.php
+++ database/factories/Partner.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Partner;
+use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 // Factory to create a basic Partner model.
@@ -9,3 +10,9 @@ $factory->define(Partner::class, function (Faker $faker) {
         'name' => $faker->company,
     ];
 });
+
+// Factory state to transform a partner to a former
+// partner, who left the network of the currency.
+$factory->state(Partner::class, 'former', [
+    'left_on' => Carbon::parse('3 months ago'),
+]);
