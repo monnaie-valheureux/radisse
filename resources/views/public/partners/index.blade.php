7f94ae0f0cf5b4ae62d898edc8b744eb86c3988f
michael <michael@estsurinter.net>
2017-10-25 23:26:35 +0200

    Document the view listing partners

--- resources/views/public/partners/index.blade.php
+++ resources/views/public/partners/index.blade.php
@@ -7,10 +7,20 @@
     <h2 class="partner-list-main-heading">Où dépenser les Val’heureux ?</h2>
 
     <div class="partner-list">
+    {{--
+        Display an alphabetically sorted list of initials. Some letters of
+        the alphabet may be missing, because we’re only listing letters
+        for those we have at least one partner name starting with it.
+    --}}
     @foreach ($initials as $letter => $partners)
         <h3 class="partner-list__sublist-label">{{ $letter }}</h3>
 
         <ul class="partner-list__sublist">
+            {{--
+                For each initial, we display an alphabetically
+                sorted list of partners who have their ‘list
+                name’ starting with this initial.
+            --}}
             @foreach ($partners as $partner)
                 <li class="partner-list__entry">
                     {{ $partner->name_sort }}
