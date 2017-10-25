dbd320e74f8d13cbe5a9a984cc396cd108e68dc2
michael <michael@estsurinter.net>
2017-10-25 23:18:20 +0200

    Add descriptions in routes file

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,16 @@
 Route::view('/', 'public.home')->name('home');
 
 
+// This group defines the routes used by the public side of the site.
 Route::namespace('Site')->group(function () {
+
+    // These are the main three entry points to the inside of the site.
+    // They match the three links in the main menu of the site.
     Route::view('/le-projet', 'public.project');
     Route::view('/comptoirs', 'public.counters.index');
     Route::get('/partenaires', 'PartnersController@index');
 
+    // A static page telling about the ‘apéros du Val’heureux’.
     Route::view('/aperos-du-valheureux', 'public.project');
 });
 
