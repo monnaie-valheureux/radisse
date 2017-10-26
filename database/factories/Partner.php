ed8695c863a1f4192db514673938931e15ebc35b
michael <michael@estsurinter.net>
2017-10-26 03:58:00 +0200

    Tweak factory state description

--- database/factories/Partner.php
+++ database/factories/Partner.php
@@ -12,8 +12,8 @@ $factory->define(Partner::class, function (Faker $faker) {
     ];
 });
 
-// Factory state to transform a partner to a former
-// partner, who left the network of the currency.
+// State to turn a partner into a former partner,
+// who left the network of the currency.
 $factory->state(Partner::class, 'former', [
     'left_on' => Carbon::parse('3 months ago'),
 ]);
