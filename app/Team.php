52c693242d49c7c38d8631e9b33696d01ab95b42
michael <michael@estsurinter.net>
2017-11-11 00:20:40 +0100

    Add `Team` & `TeamMember` Eloquent models

---
+++ app/Team.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * A team is a subgroup among the people working for the local currency.
+ */
+class Team extends Model
+{
+    /**
+     * Get the members of the team.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function members()
+    {
+        return $this->hasMany(TeamMember::class);
+    }
+}
