f9c31a7f0c92951da004563cd70d0b53a70a7ec4
michael <michael@estsurinter.net>
2017-11-12 19:13:51 +0100

    Add FR language strings for login errors

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'L’adresse e-mail ou le mot de passe est incorrect.',
+    'throttle' => 'Trop de tentatives de connexion. Merci de réessayer dans :seconds secondes.',
+
+];
