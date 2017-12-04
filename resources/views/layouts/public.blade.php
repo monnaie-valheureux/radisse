ff63006d8b0d0db4d9492419e240b8d90f450e41
michael <michael@estsurinter.net>
2017-12-04 15:28:50 +0100

    Fix capitalization of site title with a dedicated element
    In order to fix observed erratic cross-browser behaviour regarding the `first-letter` pseudo-class/element, we now use a dedicated <span> element to isolate this letter and target it with a simple class selector.
    
    While this solution is not as pure as the previous one, it is more consistent and should solve the issue once and for all.

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -50,12 +50,16 @@
         --}}
         @if (Route::is('home'))
             <h1 class="site-header__site-title">
-                <span>Le Val’heureux</span>
+                <span>
+                    <span class="site-header__first-letter">L</span>e Val’heureux
+                </span>
             </h1>
         @else
             <a href="/" class="site-header__home-link">
                 <h1 class="site-header__site-title">
-                    <span>Le Val’heureux</span>
+                    <span>
+                        <span class="site-header__first-letter">L</span>e Val’heureux
+                    </span>
                 </h1>
             </a>
         @endif
