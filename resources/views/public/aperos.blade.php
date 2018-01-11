71e9f76f7dbaad5d009118cea20d5445abaa58e5
michael <michael@estsurinter.net>
2018-01-11 20:03:59 +0100

    Update Blade view for apéros

--- resources/views/public/aperos.blade.php
+++ resources/views/public/aperos.blade.php
@@ -4,8 +4,20 @@
 
 @section('content')
 
-    <div class="le-projet">
-        <img src="{{ asset('img/under_construction--blue.svg') }}" alt="">
-        <p>Page en construction… Elle arrivera dès que possible :-)</p>
+    <div class="long-text">
+        <h2>Les apéros du Val’heureux</h2>
+        <p>Les apéros du Val’heureux ont lieu tous les troisièmes jeudis du mois <strong>de 18h à 21h</strong> dans un lieu partenaire différent. Organisés dans une ambiance conviviale, ils ont pour objectifs de :</p>
+        <ul>
+            <li>permettre au public et aux partenaires potentiels de rencontrer l’équipe et de se familiariser avec le projet ;</li>
+            <li>mettre nos partenaires à l’honneur par la création d’un événement à leur image ;</li>
+            <li>tisser et renforcer les liens entre les différents acteurs de l’économie locale.</li>
+        </ul>
+
+        <p>Dates des prochains apéros (de 18h à 21h) :</p>
+        <ul>
+            <li>le <strong>18 janvier</strong> aux <strong>Bouchers Verts</strong> (rue de la Madeleine, 33, 4000 Liège) - <a href="https://www.facebook.com/events/1906323129379947/">Voir l’évènement Facebook</a> ;</li>
+            <li>le <strong>15 février</strong> chez <strong>City Game</strong> (Rue Lonhienne, 16, 4000 Liège) ;</li>
+            <li>le <strong>15 mars</strong> au <strong>Clos du Gourmet</strong> (187A boulevard Émile de Laveleye, 4020 Liège).</li>
+        </ul>
     </div>
 @endsection
