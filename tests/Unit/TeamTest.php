bde97231517ffed1e9f5828908efc2fb2095e803
michael <michael@estsurinter.net>
2018-05-08 22:33:30 +0200

    Allow to get the partners that are managed by a team

--- tests/Unit/TeamTest.php
+++ tests/Unit/TeamTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Admin;
 
 use App\Team;
+use App\Partner;
 use App\TeamMember;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -28,5 +29,24 @@ class TeamTest extends TestCase
         // Check that we got the correct data.
         $this->assertEquals($team->id, $retrievedMembers->get(0)->team_id);
         $this->assertEquals($team->id, $retrievedMembers->get(1)->team_id);
+    }
+
+    /** @test */
+    function can_retrieve_its_partners()
+    {
+        // Create a team and two partners associated with it.
+        $team = factory(Team::class)->create();
+
+        $partners = factory(Partner::class, 2)->create([
+            'team_id' => $team->id,
+        ]);
+
+        // Retrieve the partners.
+        $retrievedPartners = $team->partners;
+
+        // Check that we got the correct data.
+        $this->assertCount(2, $retrievedPartners);
+        $this->assertEquals($team->id, $retrievedPartners->get(0)->team_id);
+        $this->assertEquals($team->id, $retrievedPartners->get(1)->team_id);
     }
 }
