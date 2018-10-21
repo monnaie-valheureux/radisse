645affc7062db16bf976a7062b5ce89ebbf50ab9
michael <michael@estsurinter.net>
2018-10-21 17:40:59 +0200

    Add migration to update `currency_exchanges` table
    This modifies the `currency_exchanges` table to add soft deletes as well as an optional string field saying why a currency exchange has stopped.

---
+++ database/migrations/2018_10_04_205801_add_soft_deletes_and_reason_for_ending_to_currency_exchanges_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSoftDeletesAndReasonForEndingToCurrencyExchangesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Modify the `currency_exchanges` table to add soft deletes as well as
+        // an optional string field saying why a currency exchange has stopped.
+        Schema::table('currency_exchanges', function (Blueprint $table) {
+            $table->softDeletes('ended_at');
+            $table->string('reason_for_ending')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Modify the `currency_exchanges` table to remove the
+        // soft deletes as well as the optional string field
+        // saying why a currency exchange has stopped.
+        Schema::table('currency_exchanges', function (Blueprint $table) {
+            $table->dropColumn('ended_at');
+            $table->dropColumn('reason_for_ending');
+        });
+    }
+}
