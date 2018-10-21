db03a9dc68ac1051a3705ddb0366f1be3333cc32
michael <michael@estsurinter.net>
2018-10-21 17:41:42 +0200

    Use soft deletes on CurrencyExchange model

--- app/CurrencyExchange.php
+++ app/CurrencyExchange.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * A currency exchange is a place where people can get bills of the
@@ -10,6 +11,15 @@ use Illuminate\Database\Eloquent\Model;
  */
 class CurrencyExchange extends Model
 {
+    use SoftDeletes;
+
+    /**
+     * The name of the ‘deleted at’ column.
+     *
+     * @var string
+     */
+    const DELETED_AT = 'ended_at';
+
     /**
      * Get the location where the currency exchange is located.
      *
