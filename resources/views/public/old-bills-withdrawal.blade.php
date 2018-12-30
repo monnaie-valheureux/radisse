fa9667a876c89c99aaee3248b2e0182e92a32cb5
michael <michael@estsurinter.net>
2018-12-30 23:02:55 +0100

    Update info about old bills withdrawal

--- resources/views/public/old-bills-withdrawal.blade.php
+++ resources/views/public/old-bills-withdrawal.blade.php
@@ -57,7 +57,7 @@
 
     <h2 id="petit-rappel">Petit rappel</h2>
 
-    <p>En octobre dernier, le « Valeureux » s’est transformé en « Val<strong>’h</strong>eureux ». Depuis, la monnaie circule dans la majorité de la province de Liège.</p>
+    <p>En octobre 2017, le « Valeureux » s’est transformé en « Val<strong>’h</strong>eureux ». Depuis, la monnaie circule dans la majorité de la province de Liège.</p>
     <p>À cette occasion, de nouveaux billets ont été mis en circulation, principalement pour deux raisons :</p>
     <ol>
         <li>Tandis qu’il y avait encore assez de billets pour que la monnaie circule dans la ville de Liège, le stock aurait été insuffisant pour couvrir la nouvelle zone élargie ;</li>
@@ -67,18 +67,16 @@
 
     <h2 id="pourquoi-retirer-les-anciens-billets-de-la-circulation">Pourquoi retirer les anciens billets de la circulation ?</h2>
 
-    <p>Avoir deux séries de billets différents en circulation, c’est un peu confus. D’autant plus que les billets n’ont pas les mêmes dimensions ni le même style graphique. Mettre les anciens billets à la retraite permettra de clarifier les choses.</p>
+    <p>Avoir deux séries de billets différents en circulation, c’est un peu confus. D’autant plus que les billets n’ont pas les mêmes dimensions ni le même style graphique. Mettre les anciens billets à la retraite permet de clarifier les choses.</p>
     <p>Ensuite, gérer deux séries en parallèle représente un certain travail pour l’ASBL du Val’heureux, et nous aimerions pouvoir libérer du temps et de l’énergie (entièrement bénévoles) pour les consacrer à des choses plus importantes pour le projet, comme par exemple le réseau de commerçants qui ne cesse de grandir.</p>
 
     <h2 id="que-se-passera-t-il-a-partir-de-septembre">Est-ce que mes billets vont perdre leur valeur ?</h2>
 
-    <p>Non. <strong>Les anciens billets de Valeureux garderont toujours leur valeur.</strong> Par contre, si vous souhaitez les échanger contre de nouveaux billets, vous devrez contacter directement l’ASBL, ou venir à l’un de nos évènements (comme par exemple les <a href="/aperos-du-valheureux">apéros du Val’heureux</a>).</p>
-
-    <p>Depuis plusieurs mois, nous passons régulièrement chez les commerçants pour y récupérer les anciens billets et les échanger contre des nouveaux. Depuis septembre, nous cessons progressivement cela. Désormais, l’ASBL est le seul lieu de reprise des anciens billets.</p>
+    <p>Non. <strong>Les anciens billets de Valeureux garderont toujours leur valeur.</strong> Mais nous vous conseillons de les échanger contre des nouveaux. Pour cela, contactez directement l’ASBL, ou venez à l’un de nos évènements (comme par exemple les <a href="/aperos-du-valheureux">apéros du Val’heureux</a>).</p>
 
     <h2 id="que-deviennent-les-anciens-billets">Que deviennent les anciens billets ?</h2>
 
-    <p>Ils sont tous rassemblés par une personne responsable au sein de l’ASBL. Ensuite, ils sont tous scrupuleusement recomptés plusieurs fois, un par un, par trois personnes différentes, puis nous les sortons officiellement de la circulation au niveau comptable. Enfin, ils sont physiquement détruits et rejoignent le paradis des monnaies locales, après presque quatre ans de bons et loyaux services :-)</p>
+    <p>Ils sont tous rassemblés par une personne responsable au sein de l’ASBL. Ensuite, ils sont tous scrupuleusement recomptés plusieurs fois, un par un, par trois personnes différentes, puis nous les sortons officiellement de la circulation au niveau comptable. Enfin, ils sont physiquement détruits et rejoignent le paradis des monnaies locales, après presque cinq ans de bons et loyaux services :-)</p>
 
     <h2 id="garder-anciens-billets-comme-souvenir">Moi j’aimais bien les anciens billets… Je peux les garder comme souvenir ?</h2>
 
