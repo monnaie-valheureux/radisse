5b5c99c237153d480a1060c26a8f1ea9b56ddc62
michael <michael@estsurinter.net>
2019-01-14 21:44:54 +0100

    Update content for `apéros`
    Starting now, this page will also lists events that are taking place outside Liège. We thus need to update text content accordingly.
    
    This commit also adds new upcoming events.

--- resources/views/public/aperos.blade.php
+++ resources/views/public/aperos.blade.php
@@ -6,13 +6,20 @@
 
     <div class="long-text">
         <h2>Les apéros du Val’heureux</h2>
-        <p>Les apéros du Val’heureux ont lieu tous <strong>les troisièmes jeudis du mois de 18h à 21h</strong> (voire plus tard !) dans un lieu partenaire différent. Organisés dans une ambiance conviviale, ils ont pour objectifs de :</p>
+        <p>Les apéros du Val’heureux ont régulièrement lieu chez différents partenaires. Organisés dans une ambiance conviviale, ils ont pour objectifs de :</p>
         <ul>
             <li>permettre au public et aux partenaires potentiels de rencontrer l’équipe et de se familiariser avec le projet ;</li>
             <li>mettre nos partenaires à l’honneur par la création d’un événement à leur image ;</li>
             <li>tisser et renforcer les liens entre les différents acteurs de l’économie locale.</li>
         </ul>
+        <p>À <strong>Liège</strong>, les apéros du Val’heureux ont lieu tous <strong>les troisièmes jeudis du mois de 18h à 21h</strong> (voire plus tard !)</p>
+        <p>Des apéros commencent aussi à être organisés ailleurs dans la province.</p>
 
-        <p><strong>Les dates et lieux des prochains apéros seront bientôt annoncés ici !</strong></p>
+        <p>Dates des prochains apéros (de 18h à 21h) :</p>
+        <ul>
+            <li>le <strong>17 janvier</strong> chez <a href="/partenaires/addict-coffee">Addict Coffee</a> (rue du Méry 18, Liège) ;</li>
+            <li>le <strong>4 février</strong> à la <a href="/partenaires/la-table-mediterraneenne">Table méditerranéenne</a> (rue de Louveigné 10, Remouchamps) et, juste à côté, à la <a href="/partenaires/boucherie-raphael-thonon">boucherie Thonon</a> ;</li>
+            <li>le <strong>21 février</strong> à <a href="/partenaires/la-chocolateria-by-cocoatree">la Chocolateria</a> (rue du Palais 3, Liège).</li>
+        </ul>
     </div>
 @endsection
