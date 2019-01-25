d62e42948607abfd0c5344326dd0aa50ac5da52e
michael <michael@estsurinter.net>
2019-01-25 23:09:18 +0100

    Add `NonGeolocatable` exception class

---
+++ app/Exceptions/NonGeolocatable.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use App\Location;
+use App\PostalAddress;
+
+class NonGeolocatable extends Exception
+{
+    public static function geocodingProvidersFoundNothing(string $address): self
+    {
+        return new self("Geocoding providers were not able to geocode address [{$address}].");
+    }
+
+    public static function markedExplicitly(PostalAddress $address): self
+    {
+        return new self("Address [{$address->toString()}] could not be geocoded because it is explicitly marked as non-geolocatable.");
+    }
+
+    public static function locationHasNoAddress(Location $location): self
+    {
+        return new self("Location [{$location->name()}] (ID {$location->id}) has no defined postal address.");
+    }
+}
