52a017da7b2c16ddefbeb6388f2083d313be8370
michael <michael@estsurinter.net>
2019-04-14 19:23:09 +0200

    Reimplement MailerLite subscriptions without SDK
    We basically replace SDK calls by lower level, Guzzle-powered HTTP requests. It’s not *that* hardcore (we’re not writing `cURL` calls here) and it is probably more efficient.

--- app/Http/Controllers/Site/NewsletterController.php
+++ app/Http/Controllers/Site/NewsletterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Site;
 use Illuminate\Http\Request;
 use MailerLiteApi\MailerLite;
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Client as GuzzleClient;
 
 /**
  * Allow people to subscribe to the newsletter.
@@ -20,20 +21,26 @@ class NewsletterController extends Controller
      */
     public function store(Request $request)
     {
-        // Instantiate a MailerLite client.
-        $mailerLiteClient = new MailerLite(config('services.mailerlite.key'));
+        // Instantiate a Guzzle client.
+        $client = new GuzzleClient([
+            'base_uri' => 'https://api.mailerlite.com/api/v2/',
+            'headers' => [
+                'X-MailerLite-ApiKey' => config('services.mailerlite.key'),
+                'Content-Type' => 'application/json',
+            ],
+        ]);
+
+        // Get the existing groups of subscribers.
+        $groups = json_decode($client->get('groups')->getBody());
 
         // Get the ID of the group of e-mail addresses that
         // is dedicated to newsletter subscriptions.
-        $groups = $mailerLiteClient->groups()->get();
-
         $newsletterGroupID = collect($groups)->firstWhere('name', 'Newsletter')->id;
 
         // Then, subscribe the address that was submitted.
-        $return = $mailerLiteClient->groups()->addSubscriber(
-            $newsletterGroupID,
-            ['email' => $email = $request->get('email')]
-        );
+        $response = $client->post("groups/{$newsletterGroupID}/subscribers", [
+            'json' => ['email' => $email = $request->get('email')],
+        ]);
 
         // Finally, return a view saying that everything is OK.
         return view('public.subscribed-to-newsletter', compact('email'));
