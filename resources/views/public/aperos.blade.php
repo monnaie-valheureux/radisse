2e5de2ae502f869f9cd5913fb1c409a270c00952
michael <michael@estsurinter.net>
2019-06-08 08:51:45 +0200

    Update content for `apéros`

--- resources/views/public/aperos.blade.php
+++ resources/views/public/aperos.blade.php
@@ -17,7 +17,7 @@
 
         <p>Dates des prochains apéros (de 18h à 21h) :</p>
         <ul>
-            <li>le <strong>18 avril</strong> chez les <a href="/partenaires/les-oiseaux-sentetent">Oiseaux s'entêtent</a> (rue Roture 13, Liège).</li>
+            <li>le <strong>16 mai</strong> à <a href="/partenaires/la-cyclerie">la Cyclerie</a> (rue de la Commune 7, Liège).</li>
         </ul>
     </div>
 @endsection
