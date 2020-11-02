a8637db88d0ddaed0a494e37a02e59874c0e1843
michael <michael@estsurinter.net>
2020-11-03 00:37:33 +0100

    Update NPM dependencies

--- resources/views/admin/partners/create/end.blade.php
+++ resources/views/admin/partners/create/end.blade.php
@@ -25,9 +25,4 @@
             Revenir à la liste de tous les partenaires
         </a>
     </p>
-
-    <h3>Un problème ? Une question ?</h3>
-
-    <p>L’essentiel de cet outil fonctionne, mais il doit encore évoluer un peu. Ça viendra. En attendant, il est déjà utilisable pour ajouter les nouveaux partenaires.</p>
-    <p>Si vous pensez avoir repéré un problème ou un bug, ou bien si vous avez une question ou si quelque chose vous a semblé pas clair, signalez-le par e-mail à Michaël (<a href="mailto:&#x6d;&#105;&#99;h&#97;&#x65;&#108;&#64;e&#x73;t&#x73;u&#114;i&#x6e;&#x74;er.&#x6e;&#101;t">&#x6d;&#105;&#99;h&#97;&#x65;&#108;&#64;e&#x73;t&#x73;u&#114;i&#x6e;&#x74;er.&#x6e;&#101;t</a>) pour qu’il puisse s’en occuper.</p>
 @endsection
